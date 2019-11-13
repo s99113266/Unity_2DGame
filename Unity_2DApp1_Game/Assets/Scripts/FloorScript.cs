7ec7eef063ce3b183d21a7d88b9ea377c5ef88cb
s99113266 <41054903+s99113266@users.noreply.github.com>
2019-11-13 21:35:57 +0800

    API認識

--- Unity_2DApp1_Game/Assets/Scripts/FloorScript.cs
+++ Unity_2DApp1_Game/Assets/Scripts/FloorScript.cs
@@ -3,6 +3,12 @@
 public class FloorScript : MonoBehaviour
 {
     [Header("滾動速度")]
-    [Range(0.5f,99f)]
+    [Range(0.1f,99f)]
     public float floorScroll = 0.5f;  //滾動速度
+    public Transform floorP;
+    
+    private void Update()
+    {
+        floorP.Translate(-floorScroll, 0, 0);
+    }
 }
---
+++ Unity_2DApp1_Game/Assets/Scripts/staticTest.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class staticTest : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+}
